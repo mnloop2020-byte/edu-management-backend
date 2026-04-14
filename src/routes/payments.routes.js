baf9eeee4f698e670a417636db901ad4ed9350ad
Mohammed AL-hafidi <mnloop2020@gmail.com>
2026-04-14 10:14:30 +0300

    add payment transactions and delete endpoint

--- src/routes/payments.routes.js
+++ src/routes/payments.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   getAllPayments,
   getPaymentsByStudent,
+  getPaymentTransactions,
   createPayment,
   addPartialPayment,
+  deletePayment,
   updatePaymentStatus,
   getPaymentsSummary,
 } = require("../controllers/payments.controller");
@@ -15,8 +17,10 @@ router.use(protect);
 router.get("/", getAllPayments);
 router.get("/summary", getPaymentsSummary);
 router.get("/student/:studentId", getPaymentsByStudent);
+router.get("/:id/transactions", getPaymentTransactions);
 router.post("/", createPayment);
 router.patch("/:id/pay", addPartialPayment);
 router.patch("/:id/status", updatePaymentStatus);
+router.delete("/:id", deletePayment);
 
 module.exports = router;
